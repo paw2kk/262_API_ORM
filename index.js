24ecf7b13464d4cca1199e50dc79dbb4fb0b7aa7
paw2k <wndxakk@gmail.com>
2026-07-20 15:30:32 +0700

    feat: add DELETE endpoint to remove a comic by ID

--- index.js
+++ index.js
@@ -46,3 +46,17 @@ db.sequelize.sync()
         }
     });
 
+    app.delete('/komik/:id', async (req, res) => {
+        const id = req.params.id;
+        try {
+            const komik = await db.Komik.findByPk(id);
+            if (!komik) {
+                return res.status(404).send({ message: 'Komik tidak ditemukan' });
+            }
+
+            await komik.destroy();
+            res.send({ message: 'Komik berhasil dihapus' });
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    });
